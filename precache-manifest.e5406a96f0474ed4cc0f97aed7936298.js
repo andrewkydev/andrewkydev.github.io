dafa3190afaaddf579ad43d3c2a16e573bbebc35
andrewkydev <grusha.belaev@gmail.com>
2023-02-11 00:35:53 +0300

    Updates

---
+++ precache-manifest.e5406a96f0474ed4cc0f97aed7936298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf299d12b4ce24e712b5",
+    "url": "/2/static/css/main.cb76abba.chunk.css"
+  },
+  {
+    "revision": "bf299d12b4ce24e712b5",
+    "url": "/2/static/js/main.0d10aada.chunk.js"
+  },
+  {
+    "revision": "92fa93f83c41e75819cf",
+    "url": "/2/static/js/runtime~main.f91ae047.js"
+  },
+  {
+    "revision": "5f3887f4d9376254579e",
+    "url": "/2/static/css/2.d58e19fe.chunk.css"
+  },
+  {
+    "revision": "5f3887f4d9376254579e",
+    "url": "/2/static/js/2.cde8d93c.chunk.js"
+  },
+  {
+    "revision": "fed5c13da12ab2e34cab7c1444b33f69",
+    "url": "/2/index.html"
+  }
+];
