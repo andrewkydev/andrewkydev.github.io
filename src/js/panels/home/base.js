6be8dce9f909fa231148f2374fd20bbcaf35a3c7
andrewkydev <grusha.belaev@gmail.com>
2023-02-11 00:33:00 +0300

    2

---
+++ src/js/panels/home/base.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { closePopout, goBack, openModal, openPopout, setPage } from '../../store/router/actions';
+
+import { Div, Panel, Alert, Group, Button, PanelHeader } from "@vkontakte/vkui"
+
+class HomePanelBase extends React.Component {
+
+    state = {
+        showImg: false
+    };
+
+    showImg = () => {
+        this.setState({ showImg: true });
+    };
+
+    openPopout() {
+        this.props.openPopout(
+            <Alert
+                actions={[{
+                    title: 'Нет',
+                    autoclose: true,
+                    style: 'cancel',
+                }, {
+                    title: 'Да',
+                    autoclose: true,
+                    action: this.showImg
+                }]}
+                onClose={() => this.props.closePopout()}
+            >
+                <h2>Вопрос значит</h2>
+                <p>Вас роняли в детстве?</p>
+            </Alert>
+        );
+    }
+
+    render() {
+        const { id, setPage, withoutEpic } = this.props;
+        const url = new URL(window.location.href);
+        const payload = async () => {
+            if (url.searchParams.get('payload') != null) return await url.searchParams.get('payload');
+            else {
+                return null;
+            }
+        }
+
+        return (
+            <Panel id={id}>
+                <PanelHeader>Examples</PanelHeader>
+                <Group>
+                    {/* <Div>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => setPage('home', 'groups')}>Список моих
+                            групп</Button>
+                    </Div> */}
+                    <Div>
+                        <p>
+                            {new URL(window.location.href).searchParams.get('payload') ? ('ваш ник: ' + new URL(window.location.href).searchParams.get('payload')) : 'ВЫ НЕ АВТОРИЗОВАНЫ'}
+                        </p>
+                    </Div>
+                    {/* <Div>
+                        <p>{params.get('payload')}</p>
+                    </Div> */}
+                    <Div>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.openPopout()}>Открыть алерт</Button>
+                    </Div>
+                    <Div>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal("MODAL_PAGE_BOTS_LIST")}>Открыть
+                            модальную страницу</Button>
+                    </Div>
+
+                </Group>
+            </Panel>
+        );
+    }
+
+}
+
+const mapDispatchToProps = {
+    setPage,
+    goBack,
+    openPopout,
+    closePopout,
+    openModal
+};
+
+export default connect(null, mapDispatchToProps)(HomePanelBase);
